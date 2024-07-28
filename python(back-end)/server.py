83b7e9caf725563ec10847d8be39ee034a38ac67
Ethan Baek <ethanbaek2005@gmail.com>
2024-07-27 22:19:22 -0700

    Added hand tracking with mediapipe and trained hand models for needed hand gestures

---
+++ python(back-end)/server.py
@@ -0,0 +1,5 @@
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
+
+
--- python/server.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask
-from flask_socketio import SocketIO, emit
-import cv2
-import mediapipe as mp
